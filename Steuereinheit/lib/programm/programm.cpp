67e34502e1ac9bd4db2ccd317c1f561fe6430907
Bastian Zötzl <bastian.zoetzl@outlook.com>
2022-05-02 23:02:57 +0200

    Added Steuereinheit

---
+++ Steuereinheit/lib/programm/programm.cpp
@@ -0,0 +1,34 @@
+#include <avr\io.h>
+#include "initialisieren.h"
+#include <util/delay.h>
+
+void derrest(){
+    int i = 1;
+        while(i == 1){
+        PORTB |= (1 << LED_R); //rote LED: Noch nicht gestartet!
+        if(PIND & (1<<SCHALT)){
+            PORTD &= ~(1 << RUPT_1); 
+            PORTD |= (1 << AUSGABE_1);
+            PORTB |= (1 << LED_OB); 
+            PORTB &= ~(1 << LED_R); 
+            PORTB |= (1 << LED_G); //Grüne LED: Läuft!
+            i = 2;
+        }
+        }
+
+        _delay_ms(500);
+        PORTB &= ~(1 << PB5); //aus LED
+
+        while(i == 2){
+        if(PIND & (1<<SCHALT)){
+            PORTD &= ~(1 << RUPT_2); 
+            PORTD |= (1 << AUSGABE_2);
+            PORTB |= (1 << LED_OB); 
+            PORTB &= ~(1 << LED_G);
+            PORTB |= (1 << LED_B); //Blaue LED: Beendet
+            i = 3;
+        }
+        }
+
+        _delay_ms(500); //Wartezeit Boot-Up
+}
---
+++ Steuereinheit/src/main.cpp
@@ -0,0 +1,12 @@
+#include "initialisieren.h"
+#include "programm.h"
+
+int main(void){
+  
+  while(1){
+    einfachnurinitialisieren(); //Initialisierung
+    derrest(); //Eigentliches Programm  
+  }
+
+  return 0;
+}  
