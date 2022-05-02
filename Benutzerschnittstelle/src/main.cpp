971c4b786f5d14e64b64481dd217afb781cd465a
Bastian Zötzl <bastian.zoetzl@outlook.com>
2022-05-02 23:16:11 +0200

    Added blinken

--- Benutzerschnittstelle/src/main.cpp
+++ Benutzerschnittstelle/src/main.cpp
@@ -1,2 +1,26 @@
 #include <Arduino.h>
 
+
+
+
+
+int main(void){
+
+    DDRB |= (1 << DDB3); //LEDs
+    DDRB |= (1 << DDB2);
+    DDRB |= (1 << DDB1);
+
+int i;
+for (int l = 0; l < 5; i++){
+
+    PORTB |= (1 << PB3);
+    PORTB |= (1 << PB2);
+    PORTB |= (1 << PB1);
+    _delay_ms(500);
+    PORTB &= ~(1 << PB3);
+    PORTB &= ~(1 << PB2);
+    PORTB &= ~(1 << PB1);
+    _delay_ms(500);
+}
+
+}
