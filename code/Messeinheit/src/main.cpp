1ffb3208eaf60eb86fead5f7307a291fdba3cae8
Fabian Bartl <fabianbartl.2003@outlook.de>
2022-05-08 23:23:17 +0200

    better output of debug, info and result

--- code/Messeinheit/src/main.cpp
+++ code/Messeinheit/src/main.cpp
@@ -1,56 +1,75 @@
 #define F_CPU 16000000UL
+#define DEBUG 0
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <util/atomic.h>
 #include <Arduino.h>
 
+// interrupt variables (for timer)
 volatile unsigned int timer_overflow_counter = 0;
 volatile unsigned int timer_start = 0, timer_end = 0;
 
+// timer variables
 unsigned int timer_max = 0xffff;
 double timer_sec = 0;
 
+// print debug and info messages
+void debug(char* msg) { if (DEBUG) Serial.println(msg); Serial.flush(); }
+void info(char* msg) { if (!DEBUG) Serial.println(msg); Serial.flush(); }
+
+// print result
+void result()
+{
+  // disable interrupts while calculate results
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    timer_sec = ((double)timer_start / (double)timer_max + timer_overflow_counter + (double)timer_end / (double)timer_max) / 4.0;
+    if (DEBUG)
+    {
+      Serial.print("overflows: "); Serial.println(timer_overflow_counter);
+      Serial.print("start:     "); Serial.println(timer_start);
+      Serial.print("end:       "); Serial.println(timer_end);
+      Serial.print("seconds:   "); Serial.println(timer_sec);
+      Serial.flush();
+    }
+    else
+    {
+      Serial.print(timer_sec);
+      Serial.println(" sec\n");
+      Serial.flush();
+    }
+  }
+}
+
+// interrupt events
 ISR(TIMER1_OVF_vect)
 {
   timer_overflow_counter++;
-
-  Serial.println("+");
-  Serial.flush();
+  debug("+");
 }
 
 ISR(INT0_vect)
 {
   timer_overflow_counter = 0;
   timer_start = TCNT1;
-
-  Serial.println("INT0");
-  Serial.flush();
+  debug("INT0");
+  info("start timer");
 }
 
 ISR(INT1_vect)
 {
   timer_end = TCNT1;
-
-  Serial.println("INT1");
-  Serial.flush();
-
-  timer_sec = ((double)timer_start / (double)timer_max + timer_overflow_counter + (double)timer_end / (double)timer_max) / 4.0;
-
-  Serial.println(timer_overflow_counter);
-  Serial.println(timer_start);
-  Serial.println(timer_end);
-  Serial.println(timer_sec);
-  Serial.flush();
-
-  // exit(0);
+  debug("INT1");
+  info("stop timer");
+  result();
 }
 
+// loop
 int main(void)
 {
   Serial.begin(9600);
-  Serial.println("start");
-  Serial.flush();
+  debug("\nstart");
 
   // setup pins
   DDRD &= ~((1 << DDD2) | (1 << DDD3));
@@ -69,16 +88,13 @@ int main(void)
 
   // enable interrupts
   sei();
-  _delay_ms(1000);
+  debug("configuration finished");
 
   // wait for interrupts
-  Serial.println("wait forever");
-  Serial.flush();
+  debug("wait forever");
   for(;;){}
 
-  Serial.println("not possible output");
-  Serial.flush();
-
+  debug("not possible output");
   return -1;
 }
 
